32dd779d1b344b1c755a2ec9a542782de849604f
adeelsabir14 <adeelwork14@gmail.com>
2025-02-20 12:44:17 +0500

    sample work

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product.module';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-home',
@@ -8,22 +9,31 @@ import { Product } from '../../models/product.module';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
   products: Product[] = [];
-  error: string = '';
-
-  constructor(private productService: ProductService) {}
-
-  ngOnInit(): void {
-    this.loadProducts();
-  }
-
-  loadProducts(): void {
-    this.productService.getProducts().subscribe(
-      (data: Product[]) => this.products = data,
-      (err) => this.error = err
-    );
-  }
-
+    error = '';
+  
+    constructor(
+      private productService: ProductService,
+      private cartService: CartService
+    ) { }
+  
+    ngOnInit(): void {
+      this.productService.getProducts().subscribe(
+        data => this.products = data,
+        err => this.error = err
+      );
+    }
+  
+    handleAddToCart(product: Product): void {
+      this.cartService.addToCart(product);
+      console.log('Product added to cart:', product);
+    }
+  
+    addToCart(product: Product): void {
+      this.cartService.addToCart(product);
+      console.log('Added to cart:', product);
+    }
 
 
 }
